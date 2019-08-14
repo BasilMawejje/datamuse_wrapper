7e7eb4ff80fd7475fa6bc052547220e6216d0b49
BasilMawejje <basil.mawejje@gmail.com>
2019-08-14 10:49:09 +0300

    Add before and after blocks to datamuse spec Add debugger to project"

--- spec/datamuse_wrapper_spec.rb
+++ spec/datamuse_wrapper_spec.rb
@@ -1,13 +1,20 @@
+require 'pry'
+
 RSpec.describe DatamuseWrapper do
+  before(:each) do
+    @uri = URI('http://api.datamuse.com/words?ml=ringing+in+the+ears')
+  end
+
   it "has a version number" do
     expect(DatamuseWrapper::VERSION).not_to be nil
   end
 
   it "queries words with meaning like 'ringing in the ears'" do
-    uri = URI('http://api.datamuse.com/words?ml=ringing+in+the+ears')
-
-    response = Net::HTTP.get(uri)
-
+    response = Net::HTTP.get(@uri)
     expect(response).to be_an_instance_of(String)
+  end
+
+  after(:all) do
+    @uri = nil
   end
 end
